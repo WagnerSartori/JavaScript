8a92b9393b5a74dfd2c0260a03b2e3a7627a1c70
WagnerSartori <wssartori@gmail.com>
2023-02-21 15:44:50 -0300

    Tipo de True e False

---
+++ truthy-falsy.js
@@ -0,0 +1,35 @@
+// boolean true or false
+const usuarioLogado = true;
+const contaPaga = false;
+
+// truthy ou falsy
+// 0 false 1 true
+
+console.log(0 == false);
+console.log("" == false);
+console.log(1 == true);
+
+// null = vazio ou nada, undefined
+
+let minhaVar; // desta forma ficara como undefined
+let varNull = null;
+
+console.log(minhaVar);
+console.log(varNull);
+
+let numero = 3;
+let texto = "Alura";
+
+console.log(typeof numero); //typeof retorna o tipo de variavel 
+console.log(typeof texto);
+
+console.log(typeof minhaVar);
+console.log(typeof varNull);
+
+
+
+
+
+
+
+ 
